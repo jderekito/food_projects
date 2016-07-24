19d2972ce63c77ebd76ae0a7a9db3fb316ea953c
jderekito <johnderekmorgan@hotmail.com>
2016-07-24 07:32:47 -0500

    adding update
    part

--- clean_snap.py
+++ clean_snap.py
@@ -10,7 +10,6 @@ name_field = 'Store_Name'
 match_filter_field = "Match_Code"
 
 # Select SNAP stores
-##snap_select = u'{0} = 1'.format(arcpy.AddFieldDelimiters(fc, filter_field))
 snap_select = u'{0} = 1'.format(arcpy.AddFieldDelimiters(fc, snap_filter_field))
 print(snap_select)
 
@@ -20,17 +19,22 @@ with arcpy.da.SearchCursor(fc, ['SHAPE@', name_field,match_filter_field],
     for row in cursor:
         # Print the name of the residential road
         print("Checking for : "+row[1]+" in the SNAP list")
-        snap_pt_geometry = row[0]
-        print snap_pt_geometry.type
-
         # Select stores with matching Match_Code
         non_snap_select = u'{0} LIKE \'{1}\''.format(arcpy.AddFieldDelimiters(fc, match_filter_field), row[2])
         print(non_snap_select)
-        #match_filter_select = u'{0} = {1}'.format(arcpy.AddFieldDelimiters(fc, match_filter_field),'100')
-        #match_filter_select = u'%s LIKE \'%s\'' % (arcpy.AddFieldDelimiters(fc, match_filter_field),'100')
 
         # Loop through each non-SNAP store
         with arcpy.da.SearchCursor(fc, ['SHAPE@', name_field,match_filter_field],
                                     where_clause=non_snap_select) as inner_cursor:
             for inner_row in inner_cursor:
                 print row[0].distanceTo(inner_row[0])
+
+                #Update the SNAP_DUB by OBJECTID
+                update_fields = ['WELL_YIELD', 'WELL_CLASS']
+                # Create update cursor for feature class
+                with arcpy.da.UpdateCursor(fc, fields) as update_cursor:
+                    for update_row in cursor:
+                        #set vals
+
+                        # Update the cursor with the updated list
+                        update_cursor.updateRow(update_row)
