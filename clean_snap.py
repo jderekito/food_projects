8939100894c1e5830d8044ddfb3da80e00541706
jderekito <johnderekmorgan@hotmail.com>
2016-07-24 22:25:31 -0500

    LN

--- clean_snap.py
+++ clean_snap.py
@@ -19,11 +19,11 @@ snap_select = u'{0} = 1'.format(arcpy.AddFieldDelimiters(fc, snap_filter_field))
 print(snap_select)
 
 # Loop through each SNAP store
-with arcpy.da.SearchCursor(fc, ['SHAPE@', name_field,match_filter_field, 'OID@'], where_clause=snap_select) as cursor:
+with arcpy.da.SearchCursor(fc, ['SHAPE@', name_field, match_filter_field, 'OID@'], where_clause=snap_select) as cursor:
     for row in cursor:
         snap_dup_checker = False
         # Print the name of the residential road
-        print("Checking for : "+row[1]+" in the SNAP list")
+        print("Checking for : "+row[2]+" in the SNAP list")
 
         # Select stores with matching Match_Code, that aren't SNAPs (this is key)
         non_snap_select = u'{0} LIKE \'{1}\' AND {2} = {3}'.format(arcpy.AddFieldDelimiters(fc, match_filter_field), row[2], snap_filter_field, 0)
@@ -33,7 +33,7 @@ with arcpy.da.SearchCursor(fc, ['SHAPE@', name_field,match_filter_field, 'OID@']
         with arcpy.da.SearchCursor(fc, ['SHAPE@', 'OID@', snap_filter_field], where_clause=non_snap_select) as inner_cursor:
             for inner_row in inner_cursor:
                 print(str(row[0].distanceTo(inner_row[0])) + " distance on OID: " + str(inner_row[1]))
-                if row[0].distanceTo(inner_row[0])<30.0:
+                if row[0].distanceTo(inner_row[0])<100.0:
                     snap_dup_select = u'{0} = {1}'.format(arcpy.AddFieldDelimiters(fc, "OBJECTID"), inner_row[1])
                     update_fields = ['SNAP_DUP']
                     with arcpy.da.UpdateCursor(fc, update_fields,where_clause=snap_dup_select) as update_cursor:
